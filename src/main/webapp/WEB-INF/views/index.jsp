ea9129297f56e8eb3d8067447cfcd527bdf7e221
dhmk47 <dhmk47@naver.com>
2022-06-29 16:08:21 +0900

    add car

--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -20,22 +20,36 @@
 			<div>
 				<h1>주차 관리 시스템입니다.</h1>
 				<p>차량을 간편하게 등록하고 등록된 건물의 출입을 편리하게 할 수 있습니다.</p>
-				<span>자세히 보기...</span>
+				<span class="show-dtl">자세히 보기...</span>
 			</div>
 			<form action="">
 				<section>
 					<div class="button-box">
 						<div class="registration-box">
-							<label for="car-number">차량번호</label>
-							<input type="text" id="car-number">
+							<div class="add-car">
+								<label for="car-number">차량번호:&nbsp</label>
+								<input type="text" id="car-number">
+							</div>
+							<div class="select-option">
+								<span>주차권:&nbsp</span>
+								<select>
+									<option>1일 주차권</option>
+									<option>3일 주차권</option>
+									<option>일주일 주차권</option>
+									<option>한달 주차권</option>
+								</select>
+							</div>
+							<div>
+								<button type="submit">등록하기</button>
+							</div>
 						</div>
-						<button type="button">차량 등록</button>
+						<button type="button" class="btn">차량 등록</button>
 					</div>
 					<div>
-						<button type="button">내 정보 보기</button>
+						<button type="button" class="btn">내 정보 보기</button>
 					</div>
 					<div>
-						<button type="button">결제하기</button>
+						<button type="button" class="btn">결제하기</button>
 					</div>
 				</section>
 			</form>
--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,40 +1,61 @@
-const btnList = document.querySelectorAll(".button-box button");
+const btnList = document.querySelectorAll(".btn");
 const showDtl = document.querySelector("main div span");
-const registrationBox = document.querySelector(".registration-box");
-const btnBox = document.querySelector(".button-box");
+const registrationBox = document.querySelector("main .registration-box");
+const btnBox = document.querySelector("main .button-box");
 const mainTopBox = document.querySelector("main > div");
 
-btnList[0].onfocus = () => {
+function btn1_click() {
 	registrationBox.classList.toggle("show-registration-box");
-	btnBox.style.width = "800px";
-	btnBox.style.height = "800px";
+	btnBox.classList.toggle("move-div-box-right");
+	/*registrationBox.style.width = "600px";
+	registrationBox.style.height = "600px";*/
+	btnBox.style.width = "600px";
+	btnBox.style.height = "600px";
 	mainTopBox.style.marginBottom = "50px";
-	
-	
 }
 
-btnList[0].onblur = () => {
+function btn1_click_remove() {
 	registrationBox.classList.toggle("show-registration-box");
-	btnBox.style.width = "100px";
-	btnBox.style.height = "100px";
+	btnBox.classList.toggle("move-div-box-right");
+	/*registrationBox.style.width = "600px";
+	registrationBox.style.height = "600px";*/
+	btnBox.style.width = "140px";
+	btnBox.style.height = "50px";
 	mainTopBox.style.marginBottom = "300px";
 }
 
-btnList[1].onfocus = () => {
+let flag = true;
+btnList[0].onclick = () => {
+	if(flag == true){
+		btn1_click();
+		flag = false;		
+	}
 	
 }
 
-btnList[1].onblur = () => {
+/*btnList[0].onblur = () => {
+	registrationBox.classList.toggle("show-registration-box");
+	btnBox.classList.toggle("move-div-box-right");
+	registrationBox.style.width = "600px";
+	registrationBox.style.height = "600px";
+	btnBox.style.width = "140px";
+	btnBox.style.height = "50px";
+	mainTopBox.style.marginBottom = "300px";
+}*/
+
+btnList[1].onclick = () => {
+	if(flag == false){
+		btn1_click_remove();
+		flag = true;	
+	}
 	
 }
 
-btnList[2].onfocus = () => {
+
+btnList[2].onclick = () => {
 	
 }
 
-btnList[2].onblur = () => {
-	
-}
 
 showDtl.onclick = () => {
 	location.href = "showDtl";
